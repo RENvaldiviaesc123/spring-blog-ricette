8c73caabb4464109e2ec46904343536b2ef7d584
renat <guitarra123primera@gmail.com>
2023-09-29 12:05:26 +0200

    Added index method, create method to ricettaController class and nuova ricettaform html and index html(FINISHED MILESTONE 1).

--- src/main/java/org/learning/java/springblogricette/controller/RicettaControler.java
+++ src/main/java/org/learning/java/springblogricette/controller/RicettaControler.java
@@ -1,9 +1,13 @@
 package org.learning.java.springblogricette.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.learning.java.springblogricette.model.Ricetta;
@@ -24,7 +28,21 @@ public class RicettaControler {
     }
 
     //METODO PER LA CREATE DI UNA RICETTA
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("ricetta", new Ricetta());
+    return "nuovaricetta";
+    }
 
+    @PostMapping("/create")
+    public String doCreate(@Valid@ModelAttribute("ricetta") Ricetta formRicetta, Model model, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("ricettaList",ricettaRepository.findAll());
+            return "nuovaricetta";
+        }
+        ricettaRepository.save(formRicetta);
+        return "redirect:/ricette";
+    }
 
 
     //METODO PER UNA UPDATE DI UNA RICETTA
